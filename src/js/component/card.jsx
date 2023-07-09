61b3ed43a5f360f2187e7b9ff0a1f57063d790ba
Kate Deras <65469487+quackate@users.noreply.github.com>
2023-07-09 13:22:14 +0000

    Card Border Added

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card = (props) => {
     return (
-        <div className="card m-3 text-center">
+        <div className="card border m-3 text-center">
         <img src="https://ro-camper.ro/imgs/inchiriat-camper-romania-rulota.jpg.png" className="card-img-top" />
         <div className="card-body">
              <h5 className="card-title">Card title</h5>
