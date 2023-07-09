69971e413de5173655aaf82b613a6e5478012bac
Kate Deras <65469487+quackate@users.noreply.github.com>
2023-07-09 15:53:09 +0000

    Edited Indentation

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -3,15 +3,15 @@ import React from "react";
 const Card = (props) => {
     return (
         <div className="card border m-3 text-center">
-        <img src="https://ro-camper.ro/imgs/inchiriat-camper-romania-rulota.jpg.png" className="card-img-top" />
-        <div className="card-body">
-             <h5 className="card-title">Card title</h5>
-             <p className="card-text">{props.description}</p>
-         </div>
-        <div className="card-footer">
-            <a href="#" className="btn btn-primary">Find Out More!</a>
+            <img src="https://ro-camper.ro/imgs/inchiriat-camper-romania-rulota.jpg.png" className="card-img-top" />
+            <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">{props.description}</p>
+            </div>
+            <div className="card-footer">
+                <a href="#" className="btn btn-primary">Find Out More!</a>
+            </div>
         </div>
-    </div>
     )
 };
 
--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -6,15 +6,15 @@ const Navbar = () => {
             <div className="container-fluid">
                 <a className="navbar-brand" href="#">Start Bootstrap</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
+                    <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
-                <div className="navbar-nav">
-                    <a className="nav-link active" aria-current="page" href="#">About</a>
-                    <a className="nav-link" href="#">Services</a>
-                    <a className="nav-link" href="#">Portfolio</a>
-                    <a className="nav-link" href="#" tabindex="-1" aria-disabled="true">Contact</a>
-                </div>
+                    <div className="navbar-nav">
+                        <a className="nav-link active" aria-current="page" href="#">About</a>
+                        <a className="nav-link" href="#">Services</a>
+                        <a className="nav-link" href="#">Portfolio</a>
+                        <a className="nav-link" href="#" tabindex="-1" aria-disabled="true">Contact</a>
+                    </div>
                 </div>
             </div>
         </nav>
